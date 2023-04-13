0b7b515bc7e5ed9af5b876d0558a3ce10a79cd0a
KIMPPT <dudghks9303@gmail.com>
2023-04-13 17:41:07 +0900

    04131741

---
+++ project-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Main from "./components/Main";
+import Infor from "./components/Infor";
+import Detail from "./components/Detail";
+import Others from "./components/Others";
+function App() {
+  return (
+    <div className="App">
+      abc
+      <Main />
+      <Infor />
+      <Detail/>
+      <Others/>
+    </div>
+  );
+}
+
+export default App;
