a96a29dfaadc9585bf8a2a5347ed21424e607e57
KIMPPT <dudghks9303@gmail.com>
2023-04-18 15:48:47 +0900

    04181548

--- project-app/src/components/Infor.jsx
+++ project-app/src/components/Infor.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import '../CSS/infor.css'
+import React from "react";
+import "../CSS/infor.css";
 export default function Infor() {
   return (
     <div>
-      <img src="" alt="제품홍보사진칸입니다." className="inforimg"/>
-      <button className='inforbutton'>더 보기</button>
+      <img
+        src="https://raw.githubusercontent.com/KIMPPT/Project/master/project-app/src/image/10002000.png"
+        alt="제품홍보사진칸입니다."
+        className="inforimg on"
+      />
+      <div className="boxbutton">
+      <button className="inforbutton">더 보기</button>
+      </div>
     </div>
-  )
+  );
 }
