119248147fde50e3f5148e66a6de39eee072ad61
KIMPPT <dudghks9303@gmail.com>
2023-04-21 13:06:32 +0900

    04211306

--- project-app/src/components/Detail.jsx
+++ project-app/src/components/Detail.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useRef } from "react";
+import { forwardRef } from "react";
 export default function Detail() {
   let ref=useRef(null);
-  return (
-    <div  ref={ref} >
-      <img
-        src="/image/1000500.png"
-        alt="상세이미지입니다"
-        className="detailimage"
-      />
-      <h3>교환 및 환불에 대한 안내</h3>
-    </div>
-  );
+  let detail=forwardRef((props,ref)=>
+  {
+    return (
+      <div  ref={ref} >
+        <img
+          src="/image/1000500.png"
+          alt="상세이미지입니다"
+          className="detailimage"
+        />
+        <h3>교환 및 환불에 대한 안내</h3>
+      </div>
+    );
+
+
+  }
+  )
 }
--- project-app/src/page/Home.jsx
+++ project-app/src/page/Home.jsx
@@ -4,7 +4,9 @@ import Main from "../components/Main";
 import Infor from "../components/Infor";
 import Detail from "../components/Detail";
 import Others from "../components/Others";
+
 export default function Home() {
+  
   return (
     <div>
       Home
