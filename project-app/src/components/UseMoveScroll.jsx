3feada268b480c1b0d10fb1a53bf89c0e7543e55
KIMPPT <dudghks9303@gmail.com>
2023-04-24 14:39:59 +0900

    04241439

--- project-app/src/components/UseMoveScroll.jsx
+++ project-app/src/components/UseMoveScroll.jsx
@@ -3,7 +3,7 @@ import {useRef} from 'react';
 function useMoveScroll(){
     let element=useRef<HTMLDivElement>(null);
     let onMoveToElement=()=>{
-        element.current?.scrollIntoView({behavior:'sommth', block:'start'})
+        element.current.scrollIntoView({behavior:'sommth', block:'start'})
     } 
     return{element,onMoveToElement};
 }
