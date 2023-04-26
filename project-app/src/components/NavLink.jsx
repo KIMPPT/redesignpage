035d57a603e79a26e43d5a2528f2acb1e3093d93
KIMPPT <dudghks9303@gmail.com>
2023-04-26 18:19:39 +0900

    042561819

--- project-app/src/components/NavLink.jsx
+++ project-app/src/components/NavLink.jsx
@@ -4,13 +4,13 @@ import '../CSS/navlink.css'
 export default function NavLink() {
   return (
     <div className='navlink'>
-        <Link to="/">메인페이지</Link>
-        <span>|||</span>
-        <Link to="/item">아이템 화면</Link>
-        <span>|||</span>
-        <Link to="/bucket">보관함 화면</Link>
-        <span>|||</span>
-        <Link to="/cash">결제 전 화면</Link>
+        <Link to="/" className='navlink'>메인페이지</Link>
+        <span className='navspan'>|||</span>
+        <Link to="/item"className='navlink'>아이템 화면</Link>
+        <span className='navspan'>|||</span>
+        <Link to="/bucket"className='navlink'>보관함 화면</Link>
+        <span className='navspan'>|||</span>
+        <Link to="/cash"className='navlink'>결제 전 화면</Link>
     </div>
   )
 }
